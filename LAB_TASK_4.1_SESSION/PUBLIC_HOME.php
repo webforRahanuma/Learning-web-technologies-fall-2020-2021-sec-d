a30ec28b6c91932b052f8281340c67c8e97e4f1a
webforRahanuma <rahanumatasnim98@gmail.com>
2020-11-05 10:04:09 -0800

    Commit

---
+++ LAB_TASK_4.1_SESSION/PUBLIC_HOME.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <title>X Company | Home</title>
+</head>
+<body>
+    <form>
+        <table border ="1">
+            <tr>
+                <td><img width="50" height="35" src="images/logo.png"></td>
+                <td><a href="public_home.html">Home</a></td>
+                <td><a href="login.html">Login</a></td>
+                <td><a href="registration.html">Register</a></td>
+            </tr>
+        </table>
+    </form>
+    <h1>Welcome to xCompany</h1>
+</body>
+</html>
