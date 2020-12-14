83a3b978bba0178d8dc83added26c1077f5e93e6
webforRahanuma <rahanumatasnim98@gmail.com>
2020-12-14 08:51:19 -0800

    Done

--- Final_Lab Task_Ajax_4.1/models/db.php
+++ Final_Lab Task_Ajax_4.1/models/db.php
@@ -1,42 +1,18 @@
 <?php
 
-function getConnection($servername, $dbuser, $dbpass, $database){
+	$servername = 'localhost';
+	$dbuser		= 'root';
+	$dbpass		= '';
+	$database	= 'ajax';
+
+	function getConnection(){
+		global $servername;
+		global $dbuser;
+		global $dbpass;
+		global $database;
 
 		$conn = mysqli_connect($servername, $dbuser, $dbpass, $database);
 		return $conn;
 	}
 
-function userValidate($id, $password){
-    
-    $sql = "select * from users where id = '$id' AND password = '$password'";
-    
-    $conn = getConnection('localhost', 'root', '', 'task');
-    
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    
-    if(count($row) > 0){
-        
-        if($row['user_type']=="User"){
-            
-           
-            return true;
-            
-        }
-        
-        elseif($row['user_type']=="Admin"){
-            
-            
-            return true;
-            
-        }
-        
-        
-    }
-    else{
-        
-        return false;
-        
-    }
-    
-}
+?>
